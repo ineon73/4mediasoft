95359452c95ce43924fc05f57626994bd02687b3
ineon73 <47298519+ineon73@users.noreply.github.com>
2019-07-10 14:23:18 +0400

    complete 2 excercise

--- index.php
+++ index.php
@@ -1,30 +1,51 @@
-<?php
-$text = "Верьте аль не верьте, а жил на белом свете Федот-стрелец,
-    удалой молодец. Был Федот ни красавец, ни урод, ни румян, ни бледен,
-    ни богат, ни беден, ни в парше, ни в парче, а так, вообче. Служба
-    у Федота — рыбалка да охота. Царю — дичь да рыба, Федоту — спасибо.
-    Гостей во дворце — как семян в огурце. Один из Швеции, другой из Греции,
-    третий с Гавай — и всем жрать подавай! Одному — омаров, другому — кальмаров,
-    третьему — сардин, а добытчик один! Как-то раз дают ему приказ: чуть свет
-    поутру явиться ко двору. Царь на вид сморчок, башка с кулачок, а злобности
-    в ем — агромадный объем. Смотрит на Федьку, как язвенник на редьку.
-    На Федьке от страха намокла рубаха, в висках застучало, в пузе заурчало, тут,
-    как говорится, и сказке начало…";
+<html>
+<form method="POST" action="" enctype="multipart/form-data">
+    <textarea name="text"></textarea>
+    <input type="file" name="filename">
+    <input type="submit"/>
+</form>
+</html>
 
+<?php
+$textArea = $_POST[text];
+$textFile = file_get_contents($_FILES['filename']['tmp_name']);
+if (!empty($textArea)) {
+    writeText(cleanText($textArea));
+}
+if (!empty($textFile)) {
+    writeText(cleanText($textFile));
+}
 function cleanText($text)
 {
     $a = explode(' ', $text); //разбиваем строку
-    $symbol = [',', '.', '!', ':', ';', '?', '...', '"', '—'];
+    $symbol = [',', '.', '!', ':', ';', '?', '...', '"', '—', ')', '('];
     $format = str_replace($symbol, '', $a); // очищаем слова в массиве от символов
     $format = array_diff($format, array('')); // удаляем пустые значения в массиве (лишние пробелы)
     foreach ($format as &$value) {
-        $value = mb_strtolower($value);
+        $value = trim(mb_strtolower($value));
     }
     return $format;
 }
 
-$format = cleanText($text);
-$result = count($format);
-print_r(array_count_values($format));
-echo "..." . PHP_EOL;
-echo "Всего слов: {$result}";
+function writeText($format)
+{
+    $result = count($format);
+    $list = (array_count_values($format));
+    $i = 0;
+    while (file_exists("csv/file{$i}.csv")) {
+        $i++;
+
+    }
+
+    $fp = fopen("csv/file{$i}.csv", 'w');
+    foreach ($list as $key => $line) {
+        $line = (array)$line;
+        $key = (array)$key;
+        $res = array_merge($key, $line);
+        fputcsv($fp, $res);
+    }
+    file_put_contents("csv/file{$i}.csv", "Всего слов,{$result}", FILE_APPEND);
+    fclose($fp);
+}
+
+?>
